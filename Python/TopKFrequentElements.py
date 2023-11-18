49d7d7175e86323449e140f27dd40a669ef011b8
Brandon Charest <b.charest019@gmail.com>
2023-11-18 12:01:09 -0500

    bucket sort

--- Python/TopKFrequentElements.py
+++ Python/TopKFrequentElements.py
@@ -2,6 +2,7 @@ from typing import List
 from heapq import heappush, heappushpop
 from collections import defaultdict
 
+
 # Time: O(n logk)
 # Space: (n)
 
@@ -19,6 +20,29 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
     return [x[1] for x in heap]
 
 
-print(topKFrequent([1, 1, 1, 2, 2, 3], 2))
+# Time: O(n)
+# Space: O(n)
+def topKFrequent_Bucket(nums: List[int], k: int) -> List[int]:
+    bucket = [[] for _ in range(len(nums))]
+    d = defaultdict(int)
+
+    for num in nums:
+        d[num] += 1
+
+    for key in d.keys():
+        freq = d[key]
+        bucket[freq].append(key)
+
+    res = []
+
+    for i in range(len(nums) - 1, -1, -1):
+        if len(res) == k:
+            break
+        if bucket[i]:
+            res.append(bucket[i][0])
+    return res
+
+
+print(topKFrequent_Bucket([1, 1, 1, 2, 2, 3], 2))
 print(topKFrequent([1, 1, 1, 2, 2, 3], 3))
 print(topKFrequent([1, 1, 1, 2, 2, 3], 1))
